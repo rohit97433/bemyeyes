0d0296401e2145fa36c3d16375ea297a19cff1e5
rohit97433 <72005993+rohit97433@users.noreply.github.com>
2022-06-19 19:57:52 +0530

    Add files via upload

--- app/src/main/java/com/example/vision/location.java
+++ app/src/main/java/com/example/vision/location.java
@@ -1,190 +1,145 @@
 package com.example.vision;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.os.BatteryManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.ResultReceiver;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-import org.w3c.dom.Text;
-
+import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
+import android.speech.tts.TextToSpeech;
 
-public class location extends AppCompatActivity {
-    float x1, x2;
-    private FusedLocationProviderClient fusedLocationClient;//One of the location APIs in google play services
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 2;//Request Code is used to check which permission called this function. // This request code is provided when the user is prompt for permission.
-    private LocationAddressResultReceiver addressResultReceiver;//receives the address results
-    private TextView currentAddTv;
-    private Location currentLocation;
-    private LocationCallback locationCallback;
-    private TextToSpeech textToSpeech;
+public class location extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+    TextToSpeech textToSpeech;
+    TextView lattitude,longitude,address,city,country;
+    Button getLocation;
+    private final static int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location);
-        addressResultReceiver = new LocationAddressResultReceiver(new Handler());
-        //A Handler allows you to send and process Message and Runnable objects
-        currentAddTv = findViewById(R.id.textView);
-        textToSpeech = new TextToSpeech(location.this, new TextToSpeech.OnInitListener() {
+        textToSpeech=new TextToSpeech(getBaseContext(),this);
+        textToSpeech.setLanguage(Locale.ENGLISH);
+        lattitude = findViewById(R.id.lattitude);
+        longitude = findViewById(R.id.longitude);
+        address = findViewById(R.id.address);
+        city = findViewById(R.id.city);
+        country = findViewById(R.id.country);
+        getLocation = findViewById(R.id.getLocation);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocation.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
-                    textToSpeech.setLanguage(Locale.getDefault());
-                    textToSpeech.setSpeechRate(1f);
-                    textToSpeech.speak("swipe left to get current location and swipe right to return in main menu", TextToSpeech.QUEUE_FLUSH, null);
-                }
+            public void onClick(View v) {
+
+                getLastLocation();
+
             }
         });
 
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(@NonNull LocationResult locationResult) {
-                currentLocation = locationResult.getLocations().get(0);
-                getAddress();
-            }
-        };
-        startLocationUpdates();//call this function to check location permission
     }
 
+    private void getLastLocation(){
 
-    @SuppressWarnings("MissingPermission")
-    private void startLocationUpdates() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new
-                            String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
-        } else {
-            LocationRequest locationRequest = new LocationRequest();
-            locationRequest.setInterval(2000);
-            locationRequest.setFastestInterval(1000);
-            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
-        }
-    }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
 
-    @SuppressWarnings("MissingPermission")
-    private void getAddress() {
-        if (!Geocoder.isPresent()) {
-            Toast.makeText(location.this, "Can't find current address, ",
-                    Toast.LENGTH_SHORT).show();
-            return;
-        }
-        Intent intent = new Intent(this, GetAllData.class);
-        intent.putExtra("add_receiver", addressResultReceiver);
-        intent.putExtra("add_location", currentLocation);
-        startService(intent);
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull
-            int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startLocationUpdates();
-            } else {
-                Toast.makeText(this, "Location permission not granted, " + "restart the app if you want the feature", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private class LocationAddressResultReceiver extends ResultReceiver {
-        LocationAddressResultReceiver(Handler handler) {
-            super(handler);
-        }
-
-        @Override
-        protected void onReceiveResult(int resultCode, Bundle resultData) {
-            if (resultCode == 0) {
-                Log.d("Address", "Location null retrying");
-                getAddress();
-            }
-            if (resultCode == 1) {
-                Toast.makeText(location.this, "Address not found, ", Toast.LENGTH_SHORT).show();
-            }
-            String currentAdd = resultData.getString("address_result");
-            showResults(currentAdd);
-        }
-    }
-
-    private void showResults(String currentAdd) {
-        currentAddTv.setText(currentAdd);
-        //textToSpeech.speak(currentAdd,TextToSpeech.QUEUE_FLUSH,null);
-    }
-
-    public boolean onTouchEvent(MotionEvent touchEvent) {
-
-        switch (touchEvent.getAction()) {
-
-            case MotionEvent.ACTION_DOWN:
-                x1 = touchEvent.getX();
-                break;
-            case MotionEvent.ACTION_UP:
-                x2 = touchEvent.getX();
-                if (x1 < x2) {
-                    String data = currentAddTv.getText().toString();
-                    if(data.isEmpty()){
-                        textToSpeech.speak("Please turn on location", TextToSpeech.QUEUE_FLUSH, null);
-                    }
-                    else {
-                        textToSpeech.speak("Your current location is " + data, TextToSpeech.QUEUE_FLUSH, null);
-                        textToSpeech.speak("swipe left to listen again or swipe right to return back in main menu", TextToSpeech.QUEUE_ADD, null);
-
-                    }
-
-                }
-                if (x1 > x2) {
-                    Intent i = new Intent(location.this,MainActivity.class);
-                    startActivity(i);
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.post(new Runnable() {
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
                         @Override
-                        public void run() {
-                            textToSpeech.speak("You are in main menu. just swipe right and say what you want", TextToSpeech.QUEUE_FLUSH,null);
+                        public void onSuccess(Location location) {
+
+                            if (location != null){
+
+
+
+                                try {
+                                    Geocoder geocoder = new Geocoder(location.this, Locale.getDefault());
+                                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                    String text=address.getText().toString();
+                                    textToSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+                                    lattitude.setText("Lattitude: "+addresses.get(0).getLatitude());
+                                    longitude.setText("Longitude: "+addresses.get(0).getLongitude());
+                                    address.setText("Address: "+addresses.get(0).getAddressLine(0));
+                                    city.setText("City: "+addresses.get(0).getLocality());
+                                    country.setText("Country: "+addresses.get(0).getCountryName());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+
+                            }
 
                         }
                     });
-                }
+
+
+        }else {
+
+            askPermission();
+
+
         }
-        return false;
+
+
     }
 
+    private void askPermission() {
+
+        ActivityCompat.requestPermissions(location.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+
+
+    }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        startLocationUpdates();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        fusedLocationClient.removeLocationUpdates(locationCallback);
-    }
-}
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
 
+        if (requestCode == REQUEST_CODE){
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+
+                getLastLocation();
+
+            }else {
+
+
+                Toast.makeText(location.this,"Please provide the required permission",Toast.LENGTH_SHORT).show();
+
+            }
+
+
+
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onInit(int status) {
+        textToSpeech.speak("Welcome to Location Identifier,Click the button below to know your location",TextToSpeech.QUEUE_FLUSH,null);
+
+        if (status != TextToSpeech.ERROR) {
+            Toast.makeText(getBaseContext(), "Success", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    }
